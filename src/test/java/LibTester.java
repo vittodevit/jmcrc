c47469a4f3aa7b63f372417b2ab866695dbcf0a4
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-10-06 22:22:50 +0200

    Moving license + fix response

--- src/test/java/LibTester.java
+++ src/test/java/LibTester.java
@@ -1,3 +1,20 @@
+/*
+This software is distributed under the Apache License 2.0
+Copyright 2020 Vittorio Lo Mele
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import it.mrbackslash.jmcrc.Jmcrc;
 
 public class LibTester {
@@ -15,7 +32,7 @@ public class LibTester {
         System.out.println("port = " + client.getPort());
         try{
             String response = client.send("time set 0");
-            System.out.println("Server response: ");
+            System.out.println("Server response: " + response);
         }catch(Exception x){
             System.out.println("Failed to send payload");
         }
