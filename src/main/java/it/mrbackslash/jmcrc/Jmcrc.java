ad26007b07b02477bb5b7b5146b1e00c73e191b6
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-10-06 17:38:00 +0200

    First working version of init

--- src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
+++ src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
@@ -3,6 +3,7 @@ import com.sun.istack.internal.NotNull;
 import java.util.Random;
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 
 /**
 * RCON Client for Minecraft servers implemented in Java
@@ -27,14 +28,14 @@ import java.net.Socket;
  */
 
 public class Jmcrc {
-    private static String chost;
-    private static int cport;
-    private static boolean loggedIn;
-    private static int requestId;
-    private static Socket socket;
-    private static OutputStream out;
-    private static DataOutputStream dos;
-    private static DataInputStream in;
+    private String chost;
+    private int cport;
+    private boolean loggedIn;
+    private int requestId;
+    private Socket socket;
+    private OutputStream out;
+    private InputStream in;
+    private byte[] dataBuffer;
 
     /**
      *
@@ -44,15 +45,14 @@ public class Jmcrc {
      * @return boolean
      * @throws UnableToConnectJmcrcException Throwed when server is unreachable
      */
-    public static boolean init(@NotNull String host, int port, @NotNull String password) throws UnableToConnectJmcrcException, IOException, InvalidPayloadJmcrcException {
+    public boolean init(@NotNull String host, int port, @NotNull String password) throws UnableToConnectJmcrcException, IOException, InvalidPayloadJmcrcException {
         if(loggedIn){
             throw new UnableToConnectJmcrcException();
         }
         try{
             socket = new Socket(host, port);
             out = socket.getOutputStream();
-            dos = new DataOutputStream(out);
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            in = socket.getInputStream();
         }catch(Exception x) {
             throw new UnableToConnectJmcrcException();
         }
@@ -62,19 +62,23 @@ public class Jmcrc {
         //assemble login packet
         byte[] loginPacket = PacketAssembler.AssemblePacket(requestId, 3, password);
         //write login packet
-        dos.write(loginPacket);
-        //reading packet length, not useful for validation of login packets
-        int useless = in.readInt();
-        useless = 0;
+        out.write(loginPacket);
+        //creating data buffer
+        dataBuffer = new byte[8192];
+        int rl = in.read(dataBuffer);
+        //copying response id to validate login
+        byte[] responseId = new byte[4];
+        System.arraycopy(dataBuffer,4, responseId, 0, 4);
         //reading response id, if is the same as request id for login, the operation was successful
-        int checkLogin = in.readInt();
-        if(checkLogin != requestId){
+        if(!Arrays.equals(PacketAssembler.intToByteArray(requestId), responseId)){
             return false;
         }
         //set class variables
         chost = host;
         cport = port;
         loggedIn = true;
+        //clean buffer and return
+        dataBuffer = new byte[8192];
         return true;
     }
 
@@ -82,7 +86,7 @@ public class Jmcrc {
      * Gets the state of connection
      * @return boolean
      */
-    public static boolean isConnected(){
+    public boolean isConnected(){
         return loggedIn;
     }
 
@@ -90,7 +94,7 @@ public class Jmcrc {
      * Gets the current server address
      * @return String
      */
-    public static String getAddress(){
+    public String getAddress(){
         return chost;
     }
 
@@ -98,7 +102,7 @@ public class Jmcrc {
      * Gets the current server port
      * @return int
      */
-    public static int getPort(){
+    public int getPort(){
         return cport;
     }
 
