c9619ce36664acd387f9065356c9ddb5168a8c6a
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-10-06 07:52:28 +0200

    Adding methods to main class, first draft for init

--- src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
+++ src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
@@ -1,4 +1,9 @@
 package it.mrbackslash.jmcrc;
+import com.sun.istack.internal.NotNull;
+import java.util.Random;
+import java.io.*;
+import java.net.Socket;
+
 /**
 * RCON Client for Minecraft servers implemented in Java
 *
@@ -20,12 +25,81 @@ package it.mrbackslash.jmcrc;
 * @see <a href="https://github.com/mrBackSlash-it/jmcrc">GitHub Repository</a>
 * @author mrBackSlash-it
  */
+
 public class Jmcrc {
-    private static String host;
-    private static int port;
+    private static String chost;
+    private static int cport;
     private static boolean loggedIn;
-
     private static int requestId;
+    private static Socket socket;
+    private static OutputStream out;
+    private static DataOutputStream dos;
+    private static DataInputStream in;
 
+    /**
+     *
+     * @param host Hostname or IP Address of the Minecraft Server
+     * @param port TCP Port where server RCON is listening, usually 25575
+     * @param password RCON Password
+     * @return boolean
+     * @throws UnableToConnectJmcrcException Throwed when server is unreachable
+     */
+    public static boolean init(@NotNull String host, int port, @NotNull String password) throws UnableToConnectJmcrcException, IOException, InvalidPayloadJmcrcException {
+        if(loggedIn){
+            throw new UnableToConnectJmcrcException();
+        }
+        try{
+            socket = new Socket(host, port);
+            out = socket.getOutputStream();
+            dos = new DataOutputStream(out);
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        }catch(Exception x) {
+            throw new UnableToConnectJmcrcException();
+        }
+        //generate request id
+        Random rand = new Random();
+        requestId = rand.nextInt(2147483647);
+        //assemble login packet
+        byte[] loginPacket = PacketAssembler.AssemblePacket(requestId, 3, password);
+        //write login packet
+        dos.write(loginPacket);
+        //reading packet length, not useful for validation of login packets
+        int useless = in.readInt();
+        useless = 0;
+        //reading response id, if is the same as request id for login, the operation was successful
+        int checkLogin = in.readInt();
+        if(checkLogin != requestId){
+            return false;
+        }
+        //set class variables
+        chost = host;
+        cport = port;
+        loggedIn = true;
+        return true;
+    }
+
+    /**
+     * Gets the state of connection
+     * @return boolean
+     */
+    public static boolean isConnected(){
+        return loggedIn;
+    }
+
+    /**
+     * Gets the current server address
+     * @return String
+     */
+    public static String getAddress(){
+        return chost;
+    }
+
+    /**
+     * Gets the current server port
+     * @return int
+     */
+    public static int getPort(){
+        return cport;
+    }
 
 }
