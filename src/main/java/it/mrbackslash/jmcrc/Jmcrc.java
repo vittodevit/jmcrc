294d87982832a504ba5041ad02fbebbcfd68b0d0
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-10-06 22:22:03 +0200

    Moving license, first draft for send() and disconnect() methods

--- src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
+++ src/main/java/it/mrbackslash/jmcrc/Jmcrc.java
@@ -1,3 +1,20 @@
+/*
+This software is distributed under the Apache License 2.0
+Copyright 2020 Vittorio Lo Mele
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package it.mrbackslash.jmcrc;
 import com.sun.istack.internal.NotNull;
 import java.util.Random;
@@ -7,22 +24,6 @@ import java.util.Arrays;
 
 /**
 * RCON Client for Minecraft servers implemented in Java
-*
-*   This software is distributed under the Apache License 2.0
-*   Copyright 2020 Vittorio Lo Mele
-*
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-*
 * @see <a href="https://github.com/mrBackSlash-it/jmcrc">GitHub Repository</a>
 * @author mrBackSlash-it
  */
@@ -46,7 +47,7 @@ public class Jmcrc {
      * @throws UnableToConnectJmcrcException Throwed when server is unreachable
      */
     public boolean init(@NotNull String host, int port, @NotNull String password) throws UnableToConnectJmcrcException, IOException, InvalidPayloadJmcrcException {
-        if(loggedIn){
+        if(isConnected()){
             throw new UnableToConnectJmcrcException();
         }
         try{
@@ -65,7 +66,8 @@ public class Jmcrc {
         out.write(loginPacket);
         //creating data buffer
         dataBuffer = new byte[8192];
-        int rl = in.read(dataBuffer);
+        //noinspection ResultOfMethodCallIgnored
+        in.read(dataBuffer);
         //copying response id to validate login
         byte[] responseId = new byte[4];
         System.arraycopy(dataBuffer,4, responseId, 0, 4);
@@ -80,6 +82,47 @@ public class Jmcrc {
         //clean buffer and return
         dataBuffer = new byte[8192];
         return true;
+    }
+
+    /**
+     * This function sends a command to a server after the connection has been initialized
+     * @param payload Command to send
+     * @return String (server response)
+     * @throws IOException Socket fail
+     * @throws NotConnectedJmcrcException You aren't connected to any server.
+     */
+    public String send(String payload) throws IOException, NotConnectedJmcrcException, InvalidPayloadJmcrcException, InvalidPacketJmcrcException{
+        if(!isConnected()){
+            throw new NotConnectedJmcrcException();
+        }
+        //assemble login packet
+        byte[] requestPacket = PacketAssembler.AssemblePacket(requestId, 2, payload);
+        //write login packet
+        out.write(requestPacket);
+        //creating data buffer
+        dataBuffer = new byte[8192];
+        //noinspection ResultOfMethodCallIgnored
+        in.read(dataBuffer);
+        byte[] bResponse = PacketDisassembler.disassemblePacket(dataBuffer, PacketDisassembler.PACKET_S_PAYLOAD);
+        return PacketDisassembler.rawPayloadToASCII(bResponse);
+    }
+
+    /**
+     * Closes the session and disconnects from the server.
+     * @throws IOException Socket fail
+     * @throws NotConnectedJmcrcException You aren't connected to any server.
+     */
+    public void disconnect() throws IOException, NotConnectedJmcrcException{
+        if(!isConnected()){
+            throw new NotConnectedJmcrcException();
+        }
+        in.close();
+        out.close();
+        socket.close();
+        chost = "";
+        cport = 0;
+        loggedIn = false;
+        dataBuffer = new byte[8192];
     }
 
     /**
